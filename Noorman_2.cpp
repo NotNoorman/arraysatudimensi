aa33ce74c6f8fd0cfd6100f179e8387974136190
NotNoorman <141219463+NotNoorman@users.noreply.github.com>
2023-08-02 15:12:43 +0700

    Add files via upload

---
+++ Noorman_2.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main()
+(
+//mendeklarasikan array dan variabel
+int A[2][2], B [2][2], C[2][2];
+
+cout << "Masukan Nilai A11 : ";
+cin>>A[0][0];
+cout << "Masukkan Nilai A12 : ";
+cin>>A[0]
+)
